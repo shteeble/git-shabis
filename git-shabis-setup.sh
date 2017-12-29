936061e1a929b882c5f4cae2cf410135519ed4eb
shteeble <34861664+shteeble@users.noreply.github.com>
2017-12-29 10:39:00 +0200

    Update git-shabis-setup.sh

--- git-shabis-setup.sh
+++ git-shabis-setup.sh
@@ -3,4 +3,9 @@
 echo Kashering Git...
 git config --global alias.vus status
 git config --global alias.chulent merge
+git config --global alias.shlep pull
+git config --global alias.shep fetch
+git config --global alias.shtup push
+git config --global alias.loshon_hora blame
+git config --global alias.chulent merge
 echo Shkuyach, Git is now Kosher.
