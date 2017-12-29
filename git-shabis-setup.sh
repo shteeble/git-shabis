ffd75ae8edb2e5b9bf456c12358c39f24cf2f9dd
shteeble <34861664+shteeble@users.noreply.github.com>
2017-12-29 10:43:30 +0200

    added sleeping

--- git-shabis-setup.sh
+++ git-shabis-setup.sh
@@ -2,10 +2,20 @@
 
 echo Kashering Git...
 git config --global alias.vus status
+sleep 1.5
+echo 10% complete
 git config --global alias.chulent merge
+sleep 1.5
+echo 30% complete
 git config --global alias.shlep pull
+sleep 1.5
+echo 50% complete
 git config --global alias.shep fetch
+sleep 1.5
+echo 70% complete
 git config --global alias.shtup push
+sleep 1.5
+echo 90% complete
 git config --global alias.loshon_hora blame
-git config --global alias.chulent merge
+sleep 1.5
 echo Shkuyach, Git is now Kosher.
