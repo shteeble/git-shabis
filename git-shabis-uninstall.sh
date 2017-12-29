0a5ac588e941fdd4727545d5dc2855bda5a53fcb
shteeble <34861664+shteeble@users.noreply.github.com>
2017-12-29 10:39:56 +0200

    Update git-shabis-uninstall.sh

--- git-shabis-uninstall.sh
+++ git-shabis-uninstall.sh
@@ -3,4 +3,9 @@
 echo Treifing up Git...
 git config --global --unset alias.vus
 git config --global --unset alias.chulent
+git config --global --unset alias.shlep
+git config --global --unset alias.shep
+git config --global --unset alias.shtup
+git config --global --unset alias.loshon_hora
+git config --global --unset alias.chulent
 echo Nebuch, Git is now Treif.
