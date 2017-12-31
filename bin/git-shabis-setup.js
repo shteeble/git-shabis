21e1e5c5bae52e5ae62bf0d9f18bd3ef65332f34
chaimsalzer <chaimsalzer@gmail.com>
2017-12-31 11:49:33 +0200

    Update git-shabis-setup.js

--- bin/git-shabis-setup.js
+++ bin/git-shabis-setup.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
 var setup = require('../lib/index.js');
-console.log(Kashering Git...)
+console.log("Kashering Git...");
 setup.configure();
-console.log(Shkuyach, Git is now kosher.)
+console.log("Shkuyach, Git is now kosher.")
