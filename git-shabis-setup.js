4408c4a1df161cf7089480739fe11c8ffd155011
shteeble <34861664+shteeble@users.noreply.github.com>
2017-12-31 10:19:27 +0200

    Update git-shabis-setup.js

--- git-shabis-setup.js
+++ git-shabis-setup.js
@@ -1,8 +1,13 @@
 const gitconfig = require('gitconfig')
  
-// Set git config values. 
-gitconfig.set({
-  'alias.mail': 'foo@example.com'
-}, {
-  location: 'local'
-})
+var configure = function() {
+ // Set git config values. 
+ gitconfig.set({'alias.vus': 'status'}, {location: 'global'});
+ gitconfig.set({'alias.chulent': 'merge'}, {location: 'global'});
+ gitconfig.set({'alias.shlep': 'pull'}, {location: 'global'});
+ gitconfig.set({'alias.shep': 'fetch'}, {location: 'global'});
+ gitconfig.set({'alias.shtup': 'push'}, {location: 'global'});
+ gitconfig.set({'alias.loshon_hora': 'blame'}, {location: 'global'});
+};
+
+exports configure = configure;
